3b71985ac66fc4b8c43fb451139efe00d3243aa6
ChuanF1998 <895952166@qq.com>
2020-04-26 03:05:43 -0400

    页面显示模块

--- src/oj_view.hpp
+++ src/oj_view.hpp
@@ -29,4 +29,30 @@ class OjView
 
             tl->Expand(html, &dict);
         }
+
+
+        //id name star desc header
+        static void ExpandOneQuestion(const Question ques, std::string& desc, std::string& header, std::string* html)
+        {
+            ctemplate::TemplateDictionary dict("question");
+            dict.SetValue("id", ques._id);
+            dict.SetValue("name", ques._name);
+            dict.SetValue("star", ques._star);
+            dict.SetValue("desc", desc);
+            dict.SetValue("header", header);
+            ctemplate::Template* tpl = ctemplate::Template::GetTemplate("./template/question.html", ctemplate::DO_NOT_STRIP);
+            tpl->Expand(html, &dict);
+        }
+
+
+        static void ExpandReason(const std::string& errorno, const std::string& reason, const std::string& stdout_reason, std::string* html)
+        {
+
+            ctemplate::TemplateDictionary dict("reason");
+            dict.SetValue("errono", errorno);
+            dict.SetValue("reason", reason);
+            dict.SetValue("stdout", stdout_reason);
+            ctemplate::Template* tpl = ctemplate::Template::GetTemplate("./template/reason.html", ctemplate::DO_NOT_STRIP);
+            tpl->Expand(html, &dict);
+        }
 };
